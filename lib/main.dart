038d6f7bfecf82622e5082ee442b2d4e8b555c50
origin <hatemsalma746@gmail.com>
2026-08-21 23:29:18 +0300

    Fix duplicate carousel_slider dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:moive_app/l10n/app_localizations.dart';
 import 'package:moive_app/provider/app_language_provider.dart';
-import 'package:moive_app/tabs/update_profile/update_profile_screen.dart';
 import 'package:moive_app/utils/app_route.dart';
+import 'package:moive_app/utils/app_theme.dart';
 import 'package:moive_app/view/forget_password_screen.dart';
 import 'package:moive_app/view/home_screen.dart';
 import 'package:moive_app/view/login_screen.dart';
@@ -13,10 +13,10 @@ import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-      MultiProvider(providers: [
-        ChangeNotifierProvider(create: (context) => AppLanguageProvider()),
-      ],
-  child: const MyApp()),
+    MultiProvider(providers: [
+      ChangeNotifierProvider(create: (context) => AppLanguageProvider()),
+    ],
+        child: const MyApp()),
   );
 }
 
@@ -41,10 +41,8 @@ class MyApp extends StatelessWidget {
         AppRoute.loginScreen : (context) => LoginScreen(),
         AppRoute.registerScreen : (context) => RegisterScreen(),
         AppRoute.homeScreen : (context) => HomeScreen(),
-        AppRoute.updateProfileScreen: (context) => UpdateProfileScreen(),
-
         AppRoute.forgetPasswordScreen : (context) => ForgetPasswordScreen(),
-        AppRoute.homeScreen : (context) => HomeScreen()
+
       },
     );
   }
