68340af6e4fde63dfa8b10108ce5ac172077fab2
MahmoudElkhadem <mahmodelkhadem17@gmail.com>
2026-09-03 01:43:07 +0300

    Create Search Screen

--- lib/services/endpoint.dart
+++ lib/services/endpoint.dart
@@ -1,5 +1,6 @@
 class Endpoint {
-  static const String endPointOfListMovie = '/list_movies.json';
+  static const String endPointOfListMovie = '/api/v2/list_movies.json';
   static const String endPointOfDetails = '/api/v2/movie_details.json';
   static const String endPointOfSuggestion = '/api/v2/movie_suggestions.json';
 }
+
